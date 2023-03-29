f741f0d665d13819a3b7101a56a5b385bee16ba6
eduditurbide <eduditurbide@gmail.com>
2023-03-29 09:45:12 -0300

    Update code
    Se actualizo el código para seguir con el curso

--- src/app/statistics/statistics.page.ts
+++ src/app/statistics/statistics.page.ts
@@ -9,6 +9,10 @@ import { StatisticsService } from '../../services/statistics.service';
 export class StatisticsPage implements OnInit {
 
   dataTable: any
+  strikers: any
+  selectedSegment: String = 'tabla'
+  successTable: Boolean = true
+  successStrikers: Boolean = true
 
   constructor(private apiStatistics: StatisticsService) { }
 
@@ -16,11 +20,22 @@ export class StatisticsPage implements OnInit {
     this.apiStatistics.getTable().subscribe( response => {
       console.log('response of getTable() from Statistics Service:', response)
       this.dataTable = response
+    }, err => {
+      console.log('Error:', err)
+      this.successTable = false
     })
 
     this.apiStatistics.getStriker().subscribe( response => {
       console.log('response of getStriker() from Statistics Service:', response)
+      this.strikers = response
+    }, err => {
+      console.log('Error:', err)
+      this.successStrikers = false
     })
+  }
+
+  changeSegment() {
+    console.log('selectedSegment', this.selectedSegment)
   }
 
 }
